bee03c972c9f71a73ce82f22cb6c7a2c64f99409
Alberto Santagostino <santagostino.alb@gmail.com>
2020-03-16 18:38:01 +0100

    Continue AirQualityStation development

--- ITAQA/core/AirQualityStation.py
+++ ITAQA/core/AirQualityStation.py
@@ -4,6 +4,8 @@
 Class AirQualityStation
 """
 
+from datetime import datetime
+
 from ITAQA.geography import Italy
 
 
@@ -12,22 +14,49 @@ class AirQualityStation():
     Represent an air quality measurament from a specific station in Italy
 
     Args:
-        station_name: Name of the station
-        region:       Italy's region
-        province:     Italy's province
+        station_name (str): Name of the station
+
+    Attributes:
+        station_name (str): Name of the station
+        region (Italy.Region): Region in which the station is located
+        province (Italy.Province): Province in which the station is located
+        comune (str): Comune in which the station is located
+        geolocation (tuple): Geographic coordinates in the format (lat, lon, alt)
+        metadata(dict): Information on the object
+        data(pandas.DataFrame): Air pollution data of the station
 
     Examples:
-        AirQualityStation("Torino Rebaudengo", "Italy.Region.PIEMONTE", "Italy.Province.TORINO")
+        AirQualityStation("Torino Rebaudengo")
 
     Raises:
         ValueError: If station_name is empty
     """
-    def __init__(self, station_name, region=None, province=None):
+    def __init__(self, station_name):
         # Validate service name
         if not station_name:
             raise ValueError("Station name cannot be empty")
         else:
             self.station_name = station_name
+
+        # Geographic information on the location of the station
+        self.region = Italy.Region.UNSET
+        self.province = Italy.Province.UNSET
+        self.comune = None
+        self.geolocation = None
+
+        # Metadata and data
+        self.metadata = {'created_on': datetime.now().strftime("%Y%m%d-%H%M%S")}
+        self.data = {}
+
+    def __repr__(self):
+        return f"AirQualityStation('{self.station_name}','{self.region}','{self.province},'{self.comune}')"
+
+    def __str__(self):
+        # TODO: Return table or extensive information, including amount of data collected
+        return f"AirQualityStation\n\nName:{self.station_name}"
+
+    def set_address(self, region=None, province=None, comune=None):
+        """Set region, province, comune of the station"""
         if not region:
             self.region = Italy.Region.UNSET
         else:
@@ -36,11 +65,16 @@ class AirQualityStation():
             self.province = Italy.Province.UNSET
         else:
             self.province = province
+        if not comune:
+            self.comune = None
 
-        self.geolocation = None
+    def set_geolocation(self, lat, lon, alt=None):
+        """Set geographic coordinates of the station"""
+        # TODO: Validate coordinates, catch invalid values
+        self.geolocation = (lat, lon, alt)
 
-        self.metadata = None
-        self.data = {}
-
-    def __repr__(self):
-        return f"AirQualityStation('{self.station_name}','{self.region}','{self.province}')"
+    def set_data(self, data_pd):
+        """Set pollution DataFrame"""
+        # TODO: Validate data before assignment, check if in the expected format
+        # TODO: Update metadata information (how many data, max date, min date, measured pollutants)
+        self.data = data_pd
