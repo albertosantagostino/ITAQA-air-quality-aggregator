3a5c04abe8175cdd929c08e9f417f922ddcbdc9c
Alberto Santagostino <santagostino.alb@gmail.com>
2020-03-19 17:39:15 +0100

    Add setter methods and improve AirQualityStation

--- ITAQA/core/AirQualityStation.py
+++ ITAQA/core/AirQualityStation.py
@@ -6,12 +6,12 @@ Class AirQualityStation
 
 from datetime import datetime
 
-from ITAQA.geography import Italy
+from ITAQA.geography import Italy, converter
 
 
 class AirQualityStation():
     """
-    Represent an air quality measurament from a specific station in Italy
+    Represent an air quality measurament from a specific sensor/station
 
     Args:
         station_name (str): Name of the station
@@ -45,29 +45,29 @@ class AirQualityStation():
         self.geolocation = None
 
         # Metadata and data
-        # TODO: Make the time in UTC Time and add 'Z' in the end
-        self.metadata = {'created_on': datetime.now().strftime("%Y%m%d-%H%M%S")}
+        # TODO: Make this UTC
+        self.metadata = {'creation': datetime.now().strftime("%Y%m%dT%H%M%S")}
         self.data = {}
 
     def __repr__(self):
         return f"AirQualityStation('{self.station_name}','{self.region}','{self.province},'{self.comune}')"
 
     def __str__(self):
-        # TODO: Return table or extensive information, including amount of data collected
-        return f"AirQualityStation\n\nName:{self.station_name}"
+        print_str = f"AirQualityStation\n\nName:\t\t{self.station_name:20}\n"
+        print_str += f"Location:\t{self.comune}, {self.province}, {self.region}\n"
+        print_str += f"Geolocation:\t{self.geolocation}\n"
+        # TODO: Print also amount of data stored and available pollutants in a compact way
+        print_str += f"Metadata:\t{self.metadata}"
+        return print_str
 
     def set_address(self, region=None, province=None, comune=None):
         """Set region, province, comune of the station"""
-        if not region:
-            self.region = Italy.Region.UNSET
-        else:
+        if region and isinstance(region, Italy.Region):
             self.region = region
-        if not province:
-            self.province = Italy.Province.UNSET
-        else:
+        if province and isinstance(province, Italy.Province):
             self.province = province
-        if not comune:
-            self.comune = None
+        if comune:
+            self.comune = comune
 
     def set_geolocation(self, lat, lon, alt=None):
         """Set geographic coordinates of the station"""
@@ -76,9 +76,8 @@ class AirQualityStation():
 
     def update_data(self, data_pd):
         """Set pollution DataFrame"""
-        # TODO: Avoid writing again data already present (if provided with new days update the dataframe)
-        # TODO: On rows the day and on columns the pollutants
+        # TODO: Don't write again data already present (if provided with new days, update the dataframe)
+        # TODO: Days on rows and pollutants on columns
         # TODO: Validate data before assignment, check if in the expected format
-        # TODO: Update metadata information (how many data, max date, min date, measured pollutants)
+        # TODO: Update metadata information (data stored, max date, min date, measured pollutants)
         self.data = data_pd
-        
