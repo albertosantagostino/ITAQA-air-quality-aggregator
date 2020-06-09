15027012ae9b69391fe42a8b99c2941481679364
Alberto Santagostino <santagostino.alb@gmail.com>
2020-06-09 22:23:32 +0200

    Add history metadata handling in AQS utils

--- itaqa/utils/AQS_utils.py
+++ itaqa/utils/AQS_utils.py
@@ -25,21 +25,32 @@ def merge_group(AQS_group):
     """Merge multiple groups of AQS and return a list of merged AQS objects"""
     merged_AQS_list = []
     for k in AQS_group:
+        # Setup new resulting AQS
         new_AQS = AirQualityStation.AirQualityStation(k)
         new_AQS.set_address(region=AQS_group[k][0].region,
                             province=AQS_group[k][0].province,
                             comune=AQS_group[k][0].comune)
+        new_AQS.metadata['premerge_history'] = {}
         frames = []
+
         for station in AQS_group[k]:
             frames.append(station.data.set_index('Timestamp'))
+            # TODO: Refactor to handle stations with more than one pollutant before merge
+            cols = station.data.columns.to_list()
+            cols.remove('Timestamp')
+            pollutant = cols[0]
+            new_AQS.metadata['premerge_history'][pollutant] = {}
+            new_AQS.metadata['premerge_history'][pollutant]['name'] = station.name
+            new_AQS.metadata['premerge_history'][pollutant]['uuid'] = station.metadata['uuid']
+            new_AQS.metadata['premerge_history'][pollutant]['creation'] = station.metadata['creation']
+            # TODO: Move geolocation in more accessible place?
+            new_AQS.metadata['premerge_history'][pollutant]['geolocation'] = station.geolocation
+        # Take first frame and merge all the others
         merged_df = frames.pop()
         for frame in frames:
             merged_df = merged_df.merge(frame, how='outer', left_index=True, right_index=True)
         merged_df.reset_index(inplace=True)
         new_AQS.data = merged_df
-
-        # TODO: Merge metadata and geolocation
-
         merged_AQS_list.append(new_AQS)
 
     return merged_AQS_list
