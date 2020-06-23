0c553d79b960eec57b73858fbee6d8a933ad21df
Alberto Santagostino <santagostino.alb@gmail.com>
2020-06-23 17:44:30 +0200

    Add test, add test_data, move utils functions

--- itaqa/test/test_AQS.py
+++ itaqa/test/test_AQS.py
@@ -4,8 +4,9 @@
 Tests for AirQualityStation objects
 """
 
-from itaqa.core import AirQualityStation
-
+from itaqa.core.AirQualityStation import AirQualityStation
+from itaqa.utils.serialization_utils import load_AQS_from_msgpack, dump_AQS_to_msgpack
+from itaqa.test import utils
 from datetime import datetime, timedelta
 import pandas as pd
 
@@ -14,7 +15,7 @@ import pytest
 
 @pytest.fixture
 def dummy_AQS():
-    AQS = AirQualityStation.AirQualityStation('Mount Doom')
+    AQS = AirQualityStation('Mount Doom')
     AQS.set_address(comune='Mordor')
     AQS.set_geolocation(lat=-39.15683, lng=175.6315464, alt=291)
     dts = [datetime(year=1970, month=1, day=1, hour=6) + timedelta(hours=i) for i in range(1, 30, 2)]
@@ -26,19 +27,24 @@ def dummy_AQS():
 
 
 def test_serialization(dummy_AQS):
+    """
+    Check if serialiaztion is currently working properly
+    [Fail] If msgpack encoding/decoding is broken
+    """
     # Encode AQS to msgpack
     encoded_AQS = AirQualityStation.encode_msgpack(dummy_AQS)
     # Decode msgpack to AQS
     decoded_AQS = AirQualityStation.decode_msgpack(encoded_AQS)
     # Check equality
-    check_AQS_equality(dummy_AQS, decoded_AQS)
+    utils.check_AQS_equality(dummy_AQS, decoded_AQS)
 
 
-def check_AQS_equality(AQS1, AQS2):
-    assert AQS1.name == AQS2.name
-    assert AQS1.region.value == AQS2.region.value
-    assert AQS1.province.value == AQS2.province.value
-    assert AQS1.comune == AQS2.comune
-    assert AQS1.geolocation == AQS2.geolocation
-    assert AQS1.metadata == AQS2.metadata
-    assert (AQS1.data.values == AQS2.data.values).all()
+def test_serialization_regression_load():
+    """
+    Check if old serialized stations are still loadable
+    [Fail] If msgpack serialization format changed
+    """
+    try:
+        load_AQS_from_msgpack('itaqa/test/test_data/AQS_MountDoom.msgpack')
+    except Exception:
+        pytest.fail("myFunc() raised ExceptionType unexpectedly!")
